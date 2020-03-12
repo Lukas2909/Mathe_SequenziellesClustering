f0a0dac1135ca22a58e0477d8b92b70de17a3f50
Lukas <lk.schrozberg@gmail.com>
2020-03-12 22:11:30 +0100

    0.9 Eindimensional funktioniert mit ersten 2 Distanzmodi

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args){
@@ -5,6 +7,18 @@ public class Application {
 
         // Eingabe viele Viele Cluster erwünscht
 
+        int [] x = {1,8,15,123,187,200,10,9,4,2,5};
+        List<Cluster> list= SequentialClustering.cluster(3,x,new CompleteLinkage());
+        for (Cluster c:list
+             ) {
+            System.out.print("{  ");
+            for (Data d:c.getData()
+                 ) {
+                System.out.print(d.getValue()+"  ");
+            }
+            System.out.print("}");
+            System.out.println();
 
+        }
     }
 }
--- src/SequentialClustering.java
+++ src/SequentialClustering.java
@@ -5,8 +5,35 @@ import java.util.List;
 public class SequentialClustering {
     public static List<Cluster> cluster(int amountOfClusters, int[] data, IDistanceStrategy distanceStrategy){
         List<Cluster> clusters= new ArrayList<Cluster>();
+        for(int i=0;i<data.length;i++){
+            clusters.add(new Cluster(new Data(data[i])));
+        }
+
+        while((clusters.size()>amountOfClusters)&&(clusters.size()>1)){
+            int smallestDist=Integer.MAX_VALUE;
+            int [] clusterCombination=new int[2];
+            for(int i=0; i<clusters.size()-1;i++)
+            {
+                for(int j=i+1; j<clusters.size();j++){
+                    int distance=distanceStrategy.determineDistance(clusters.get(i), clusters.get(j));
+                    if(distance<smallestDist){
+                        smallestDist=distance;
+                        clusterCombination[0]=i;
+                        clusterCombination[1]=j;
+                    }
+                }
+            }
+
+            clusters.get(clusterCombination[0]).combineWith(clusters.get(clusterCombination[1]));
+            if(clusters.get(clusterCombination[1]).isEmpty()){
+                clusters.remove(clusterCombination[1]);
+            }
+        }
 
 
+
+
+        return clusters;
     }
 
 }
